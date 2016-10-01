7ce589ddf3e53ea5bfd44db28a8c7ab96bc9a148
Franz Holzinger <holzinger@ttproducts.de>
2016-10-01 16:38:39 +0200

    Remove the German labels from marker/locallang.xml. They must be downloaded from the TYPO3 Translation Server.

--- model/field/class.tx_ttproducts_field_image.php
+++ model/field/class.tx_ttproducts_field_image.php
@@ -69,5 +69,3 @@ if (defined('TYPO3_MODE') && $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['
 	include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/tt_products/model/field/class.tx_ttproducts_field_image.php']);
 }
 
-
-?>
