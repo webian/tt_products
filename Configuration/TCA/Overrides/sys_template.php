d51b437cb448844f10b295c8729adf9cbf5a0417
Franz Holzinger <holzinger@ttproducts.de>
2020-05-02 15:29:22 +0200

    -

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -6,7 +6,7 @@ call_user_func(function () {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile( TT_PRODUCTS_EXT, 'Configuration/TypoScript/PluginSetup/Main/', 'Shop System');
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(TT_PRODUCTS_EXT, 'Configuration/TypoScript/PluginSetup/Int/',  'Shop System Variable Content');
 
-    if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility:isLoaded( 'searchbox')) {
+    if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded( 'searchbox')) {
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(TT_PRODUCTS_EXT, 'Configuration/TypoScript/PluginSetup/Searchbox/', 'Shop System Search Box');
     }
 });
